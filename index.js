2638e15ce1056e2e953428b505e562b28475a1af
shwetapund <pundshweta2020@gmail.com>
2023-12-23 02:46:09 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+const PORT = 5000;
+
+app.listen(PORT, ()=>{
+    console.log(`server is running on ${PORT}`)
+})
