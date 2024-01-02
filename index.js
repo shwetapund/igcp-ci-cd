825049a480578491a22c4df41a916b9ed899e38f
shwetapund <pundshweta2020@gmail.com>
2024-01-01 20:09:51 -0800

    git api create

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ app.use(express.json());
 
 const PORT = 5000;
 
+app.get('/', (req,res)=>{
+    res.send(`<h1>All Good</h1>`)
+})
+
+app.get('/text',(req,res)=>{
+    res.send(`<h1>Hey I am working</h1>`)
+})
+
 app.listen(PORT, ()=>{
     console.log(`server is running on ${PORT}`)
 })
